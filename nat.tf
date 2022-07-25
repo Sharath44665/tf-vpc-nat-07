24b3e8314835b90dc5060c65ed0c6f9cb7334a20
Sharath-Win64 <sharathwelcomesyouin@gmail.com>
2022-07-25 12:20:09 +0530

    fix typo error

--- nat.tf
+++ nat.tf
@@ -14,12 +14,10 @@ resource "aws_nat_gateway" "nat-gw" {
 # VPC setup for nat
 resource "aws_route_table" "main-private" {
   vpc_id = "${aws_vpc.main.id}"
-  route = [ {
-    cidr_block = "0.0.0.0/0"
-    
-    nat_gateway_id = "${aws_nat_gateway.nat-gw.id}"
-   
-  } ]
+   route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat-gw.id
+  }
 
   tags = {
     "Name" = "main-private-1"
--- vpc.tf
+++ vpc.tf
@@ -86,7 +86,7 @@ resource "aws_internet_gateway" "main-gw" {
 
 # route table
 resource "aws_route_table" "main-public" {
-  vpc_id = "${vpc_id.main.id}"
+  vpc_id = "${aws_vpc.main.id}"
   route {
     cidr_block = "0.0.0.0/0"
     gateway_id = "${aws_internet_gateway.main-gw.id}"
